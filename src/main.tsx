72c2d024918d85aea0d5b4e05005eb8f738e52bd
Mykhailo Kloz <micha21cloz@gmail.com>
2026-04-03 17:56:01 +0100

    feat: initialize Vercel Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -13,6 +14,7 @@ if (!rootElement.innerHTML) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <Analytics />
       </QueryClientProvider>
     </StrictMode>,
   );
